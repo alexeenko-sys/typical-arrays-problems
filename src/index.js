c8974bab0703d47ce7553f636d519560c6c7225d
alexeenko-sys <i.html06@gmail.com>
2021-08-01 09:38:13 +0600

    feat: add decision of the  task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array.isArray || array.length == 0 ) return 0;
+  let min = 1;
+  array.forEach(element => {
+    if(element < min){
+      min = element;
+    }
+  });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array.isArray || array.lenfth == 0) return 0;
+  let max = 1;
+  array.forEach(element => {
+    if(element > max){
+      max = el;
+    }
+  });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array.isArray || array.length == 0) return 0;
+  let avg = 0;
+  array.forEach(element => {
+    avg += element;
+  });
+  return avg / array.length;
 }
